200baab49938df0fee6996cd71474d4cb3e39ee2
심성진-PC\심성진 <kumoh130715@gmail.com>
2018-03-17 23:18:24 +0900

    페이스북 챗봇 프로젝트 시작
    http://git.kumoh.ac.kr/jinwoo/ospbot
     에서 클론 후 시작

---
+++ app.js
@@ -0,0 +1,140 @@
+'use strict';
+
+const config = require('./config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+
+
+// Messenger API parameters
+if (!config.FB_PAGE_TOKEN) {
+	throw new Error('missing FB_PAGE_TOKEN');
+}
+if (!config.FB_VERIFY_TOKEN) {
+	throw new Error('missing FB_VERIFY_TOKEN');
+}
+
+app.set('port', (process.env.PORT || 80))
+
+//serve static files in the public directory
+app.use(express.static('public'));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+
+
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    console.log("request");
+	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === config.FB_VERIFY_TOKEN) {
+		res.status(200).send(req.query['hub.challenge']);
+	} else {
+		console.error("Failed validation. Make sure the validation tokens match.");
+		res.sendStatus(403);
+	}
+})
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+  console.log("Webhook received :: " + data);
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+  
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+ console.log(messageText);
+  if (messageText) { 
+
+        sendTextMessage(senderID, messageText);
+    
+  } 
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: config.FB_PAGE_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    FB_PAGE_TOKEN: '',
+    FB_VERIFY_TOKEN: '',
+};
